48850336e9560fffce3f22ccc25d2dcd106f5ba0
luise-go <luise.goede@yahoo.de>
2021-06-02 12:06:32 +0200

    :fire: Remove comment

--- src/app/pages/MovieDetails/MovieDetails.tsx
+++ src/app/pages/MovieDetails/MovieDetails.tsx
@@ -10,8 +10,6 @@ import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
 import NavBar from '../../components/NavBar/NavBar';
 import { Movie } from '../../../types';
 
-// const categoriesArray = ['Movie', 'Adventure', 'Comedy', 'Family'];
-
 function MovieDetails(): JSX.Element {
   const { id } = useParams<{ id: string }>();
   const [movie, setMovie] = useState<Movie | null | undefined>();
@@ -25,7 +23,7 @@ function MovieDetails(): JSX.Element {
     return <div>Loading...</div>;
   }
 
-  const genre = movie.genres.map((genres) => genres.name);
+  const genreNames = movie.genres.map((genre) => genre.name);
 
   return (
     <div className={styles.container}>
@@ -36,7 +34,7 @@ function MovieDetails(): JSX.Element {
         <div className={styles.MovieTrailer}>
           <img src={movie.posterPath} alt="" />
           <p>{movie.title}</p>
-          <NavigationGenre categories={genre} />
+          <NavigationGenre categories={genreNames} />
         </div>
         <Rating value={4} />
         <p className={styles.movieDescription}>{movie.tagline}</p>
